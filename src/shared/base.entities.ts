0400d92753f0ec9208b2ee46b3a4937026a0d7b0
LordCrainer <carlosgarcia.cagm@gmail.com>
2025-05-17 22:37:29 -0500

    feat(types): add Pagination and ApiResponse interfaces for improved data handling

--- src/shared/base.entities.ts
+++ src/shared/base.entities.ts
@@ -4,3 +4,18 @@ export interface IBaseEntity {
   updatedAt?: Date
   deletedAt?: Date | null
 }
+
+export interface Pagination {
+    total: number
+    page: number
+    limit: number
+    totalPages: number
+    skip?: number
+}
+  
+export interface ApiResponse<T> {
+    status?: number
+    message?: string
+    data?: T
+    pagination?: Pagination
+}
