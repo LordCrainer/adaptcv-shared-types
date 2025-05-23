e6d0c170e401d11a6e5d529e211850ab4a9487a6
LordCrainer <carlosgarcia.cagm@gmail.com>
2025-05-23 17:15:43 -0500

    feat: add authProvider field to IUsers interface

--- src/types/users.ts
+++ src/types/users.ts
@@ -1,12 +1,14 @@
 import { IBaseEntity } from '../shared/base.entities'
 
 export type UserStatus = 'active' | 'inactive' | 'pending'
+export type UserAuthProvider = 'local' | 'google'
 export interface IUsers extends IBaseEntity {
   _id: string
   name: string
   email: string
   password?: string
   passwordHash?: string
+  authProvider?: UserAuthProvider
   timezone?: string
   isSuperAdmin?: boolean
   status: UserStatus
